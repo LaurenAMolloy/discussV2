ae478b5fbe28dd227ab9c73f2691016df7285f42
lauren_m <laurenamolloy@gmail.com>
2025-12-24 16:21:27 +0000

    Auth set up complete

--- src/auth.ts
+++ src/auth.ts
@@ -8,7 +8,7 @@ const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
 //If we do not have the credentials throw an error
-if(!GITHUB_CLIENT_ID || GITHUB_CLIENT_SECRET) {
+if(!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
     throw new Error("Missing github oath credentials")
 }
 
