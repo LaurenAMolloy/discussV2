ff852c7de67253e47ebe193fdd044d65a452f210
lauren_m <laurenamolloy@gmail.com>
2025-12-24 12:51:03 +0000

    Setting up oauth

---
+++ src/auth.ts
@@ -0,0 +1,34 @@
+import NextAuth from 'next-auth'
+import Github from 'next-auth/providers/github'
+//Prisma adapter reaches into the db and creates a user record
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { db } from "@/db"
+
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+//If we do not have the credentials throw an error
+if(!GITHUB_CLIENT_ID || GITHUB_CLIENT_SECRET) {
+    throw new Error("Missing github oath credentials")
+}
+
+//CONFIG OBJECT TO RETURN PROPERTIES ACROSS OUR APP!
+//We have destructured all of the properties required to signin etc
+export const { handlers: { GET, POST }, auth, signOut, signIn } = NextAuth({
+    adapter: PrismaAdapter(db),
+    providers: [
+        Github({
+            clientId: GITHUB_CLIENT_ID,
+            clientSecret: GITHUB_CLIENT_SECRET
+        })
+    ],
+    callbacks: {
+        //Usually not needed, fixing a bug!
+        async session({ session, user }: any){
+            if(session && user) {
+                session.user.id = user.id
+            }
+            return session;
+        }
+    }
+})
